0c3cb6bb3f031561431a8e4ea8856e2768368565
imamanjangra <imamanjangra@gmail.com>
2026-02-07 13:22:18 +0530

    Add properties schema for MongoDB model

---
+++ Backend/src/Models/properties.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const propertiesSchema = new mongoose.Schema(
+  {
+    images: [
+      {
+        url: String,
+        public_id: String,
+      },
+    ],
+    propertyName: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    propertyType: {
+      type: String,
+      enum: ["Plot", "Home", "Floor", "Villa"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Properties = mongoose.model("Properties", propertiesSchema);
