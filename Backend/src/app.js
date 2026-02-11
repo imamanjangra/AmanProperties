d006e423eeed99889981ab467f11e095c8115dae
imamanjangra <imamanjangra@gmail.com>
2026-02-12 01:16:46 +0530

    tpo fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173","http://localhost:5174" , "https://amanproperties.onrender.com" , "https://amanproperties-admin.onrender.com/"
+      "http://localhost:5173","http://localhost:5174" , "https://amanproperties.onrender.com" , "https://amanproperties-admin.onrender.com"
     ],
     credentials: true,
   })
