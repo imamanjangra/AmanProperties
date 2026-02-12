87f2ba37c8bc73269deb6938b078665953b94a51
imamanjangra <imamanjangra@gmail.com>
2026-02-12 13:03:16 +0530

    inc limit uplode

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,8 +13,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16KB" }));
-app.use(express.urlencoded({ extended: true, limit: "16KB" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 import formRoute from './Routes/formRoute.js'
