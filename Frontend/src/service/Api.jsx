7389845bf246fc42c7e165320c92cdfd68eb5fc8
imamanjangra <imamanjangra@gmail.com>
2026-02-12 18:10:32 +0530

    local path chage

--- Frontend/src/service/Api.jsx
+++ Frontend/src/service/Api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL,
-     baseURL : "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+    //  baseURL : "http://localhost:8000/api/v1",
   withCredentials: true,
 });
 
