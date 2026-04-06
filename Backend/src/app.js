a882f0d745e618c03e3e9bbe3fe5bc1eb1286d8a
imamanjangra <imamanjangra@gmail.com>
2026-04-06 18:00:31 +0530

    feat: Update CORS configuration to include new frontend URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin: [
       // "http://localhost:5173","http://localhost:5174" 
-      "http://localhost:5173","http://localhost:5174" , "https://amanproperties.onrender.com" , "https://amanproperties-admin.onrender.com"
+      "http://localhost:5173" ,"http://localhost:3000" ,"http://localhost:5174" , "https://amanproperties.onrender.com" , "https://amanproperties-admin.onrender.com" , "https://aman-properties.vercel.app"
     ],
     credentials: true,
   })
